f7246c040f48c9aca081bcdc6b602fdb095b77ea
Jaydeep Shelake <jaydeepshelake10@gmail.com>
2022-01-30 12:40:49 +0530

    added build folder

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,9 @@ if(process.env.NODE_ENV==='production'){
   );
 } else {
   app.get('/', (req, res) => {
-    res.send('API is running....');
+    res.sendFile(
+        path.resolve(__dirname, '../frontend', 'build', 'index.html')
+      )
   });
 }
 
