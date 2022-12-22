b43de9da1a93b8f0bd0b34d128badacc5c3db170
Jaydeep Shelake <jaydeepshelake10@gmail.com>
2022-12-22 15:03:35 +0530

    added render api

--- frontend/src/apis/pizza.js
+++ frontend/src/apis/pizza.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://riocafe.onrender.com',
 });
 
 //"start": "node ./backend/server.js",
